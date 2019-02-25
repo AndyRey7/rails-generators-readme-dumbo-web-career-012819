151cc83b59cb3e5318763bef3b4508da478bc74b
andy reyes <11710reyes@gmail.com>
2019-02-25 22:05:47 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190225220344) do
+ActiveRecord::Schema.define(version: 20190225220538) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(version: 20190225220344) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "category_id"
+    t.string   "published_status"
   end
 
 end
